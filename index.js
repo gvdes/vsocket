100cbadba48af95e59e853c35844b13b4aa5c6c7
lalokob <lalo.kob@gmail.com>
2021-08-10 19:25:00 -0500

    Reestructuracion de socket, para funcionar con preventa

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-/**
- * npm run dev
- */
+const { DH_UNABLE_TO_CHECK_GENERATOR } = require('constants');
+
 // ❰❰❰❰❰ D E S A R R O L L O ❱❱❱❱❱
 // const allowedSites = [ 'http://192.168.10.33:24700', 'http://localhost:24700' ];
 
 // ❰❰❰❰❰ P R O D U C C I O N ❱❱❱❱❱
-// const allowedSites = [ 'http://192.168.10.15:7007','http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4546' ];
+const allowedSites = [ 'http://192.168.10.15:7007','http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4546' ];
 
 // ❰❰❰❰❰ P R U E B A S ❱❱❱❱❱
-const allowedSites = [ 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4540' ];
+// const allowedSites = [ 'http://mx100-cedis-vtbbdhgjzk.dynamic-m.com:4540' ];
 
 console.log("Reiniciado...");
 var app = require('express')();
@@ -23,7 +22,44 @@ const resurtidos = io.of('/resurtidos');
 
 let time = (time)=>{ return `${time.getFullYear()}-${time.getMonth()}-${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`; };
 
-io.on('connection',con=>{ console.log("❰❰❰❰❰ Nueva conexion al socket ❯❯❯❯❯"); });
+io.on('connection', socket =>{
+    let clients = [];
+    console.log("❰❰❰❰❰ Nueva conexion al socket ❯❯❯❯❯");
+    console.log(socket.id);
+    
+    // socket.emit('socketid',{socketid:socket.id});
+
+    // socket.on('session_start', ({profile,socketid,from}) => {
+    //     console.log(`Validando conexion para ${socketid} (${socket.id})...`);
+
+    //     socket.profile = profile.me;
+    //     socket.brorigin = from;
+
+    //     for( let [id,socket] of io.of("/").sockets){ console.log(`${socket.id} || ${id} || ${socketid}`); }
+    // });
+
+    // socket.on('list_sockets', () => {
+    //     let sockets = [];
+    //     for( let [id,socket] of io.of("/").sockets){
+    //         console.log(socket.user);
+    //         sockets.push({
+    //             id,
+    //             socket
+    //         });
+    //     }
+    //     socket.emit('list_sockets', sockets);
+    // });
+
+    // socket.on('disconnect', () => {
+    //     console.log("\n\n\n\n\n=============================================\n\n\n\n\n");
+    //     console.log(`${socket.id} abandono el canal`);
+    //     console.log("\n\n\n\n\n=============================================");
+    // });
+
+    // socket.on('on_preventa', data =>{
+    //     console.log(`[${time(new Date())} GBSKT]: usuario unido a preventa.`);
+    // });
+});
 
 counters.on('connection',counter=>{
     console.log("\n\n\n\n\n=============================================");
@@ -79,46 +115,117 @@ counters.on('connection',counter=>{
     });
 });
 
-preventa.on('connection',pv=>{
+preventa.on('connection', socket =>{
     console.log("\n\n\n\n\n=============================================");
     console.log(`[${time(new Date())}]: ❰❰❰❰❰ Nueva conexion PREVENTA ❯❯❯❯❯`);
+    let clients = [];    
+    
+    // socket.emit('socketid',socket.id);
 
-    pv.on('joinat',gdata=>{
-        // console.log(gdata);
-        let prevGlobalRoom = `PRV-${gdata.workpoint.alias}`;//canal global de preventa
+    socket.on('join', ({ profile, workpoint, room }) => {
+        let nick = profile.me.nick;
+        let branch = workpoint.alias;
+        let _room = `PRV_${workpoint.alias}_${room}`;
 
-        pv.join(prevGlobalRoom);
-        console.log(`[${time(new Date())}]: ${gdata.me.nick} de ${gdata.workpoint.alias} se unio a ${prevGlobalRoom}`);
-        console.log("\n=============================================");
+        socket.join(_room);
+        socket.emit('joinedat', { profile, workpoint, room:_room });
+        socket.to(_room).emit('newjoin', { profile, workpoint, room:_room });
 
-        preventa.in(prevGlobalRoom).emit( 'joined', {from:gdata} );
+        console.log("\n\n\n===========");
+        console.log(`[${time(new Date())}]: ❯❯❯❯❯ ${nick} de ${branch} se unio a ${_room}`);
+        console.log("\n\===========");
     });
 
-    pv.on('order_creating', gdata =>{
-        console.log("Creando pedido");
-        let room = `PRV-${gdata.room}`;
-        let roomdash = `DASHPRV-${gdata.room}`;
-        let user = gdata.user;
+    socket.on('unjoin', ({ profile, workpoint, room }) => {
+        let nick = profile.me.nick;
+        let branch = workpoint.alias;
+        let _room = `PRV_${workpoint.alias}_${room}`;
+
+        socket.emit('unjoined', { profile, workpoint, room:_room });
+        socket.to(_room).emit('socketunjoined', { profile, workpoint, room:_room });
+        socket.leave(_room);
+
+        console.log("\n\n\n===========");
+        console.log(`[${time(new Date())}]: ❯❯❯❯❯ ${nick} de ${branch} salio de ${_room}`);
+        console.log("\n\n===========");
+    });
+
+    // socket.on('joinrooms', gdata => {
+    //     console.log(socket.id);
+    //     let branch = gdata.workpoint.alias;
+    //     let rol = gdata.profile.me._rol;
+
+    //     //ROOMS disponibles
+    //     let avlrooms = [
+    //         { name:'main', room:`PRV_${branch}`, auths:[1,2,3,4,5,6,7] },
+    //         { name:'_sales', room:`PRV_${branch}_sales`, auths:[1,2,3,4] },
+    //         { name:'_checkin', room:`PRV_${branch}_checkin`, auths:[1,2,3,4,6,7] },
+    //         { name:'_warehouse', room:`PRV_${branch}_warehouse`, auths:[1,2,3,4,5,6,7] },
+    //         { name:'_validation', room:`PRV_${branch}_validation`, auths:[1,2,3,5,6,7] },
+    //         { name:'_cashdesk', room:`PRV_${branch}_cashdesk`, auths:[1,2,3,5] },
+    //         { name:'_admin', room:`PRV_${branch}_admin`, auths:[1,2,3] },
+    //     ];
+
+    //     avlrooms.forEach( room => {
+    //         if(room.auths.findIndex( auth => auth==rol )>=0){
+    //             socket.join(room.room);//une al canal
+    //             console.log(`${gdata.profile.me.nick} se unio a ${room.room}`);
+    //             socket.emit('joinedat', {
+    //                 profile:gdata.profile, workpoint:gdata.workpoint, room:room.room
+    //             });//emite al mismo socket a los canales a los que se unio
+    //             socket.in(room.room).emit('newjoin',{
+    //                 profile:gdata.profile, workpoint:gdata.workpoint, room:room.room
+    //             });
+    //         }
+    //     });
+    // });
+
+    socket.on('order_created', data => {
+        let order = data.order.id;
+        let by = data.order.created_by.nick;
+        let branch = data.order.from.alias;
+
+        console.log(`[${time(new Date())}]: ❯❯❯❯❯ ${by} de ${branch} creo el pedido ${order}!!`);
+        socket.in(`PRV_${branch}_admin`).emit('order_created', data);
+    });
+
+    socket.on('order_changestate', data => {
+        console.log(`[${time(new Date())}]: ❯❯❯❯❯ Una orden ha cambiado!!`);
+        console.log(data);
+        let order = data.order.id;
+        let newstate = data.newstate;
+        let branch = data.order.from.alias;
+
+        console.log(branch);
+
+        switch (newstate.id) {
+            case 3: room = `PRV_${branch}_checkin`; break;
+
+            case 4:
+                /**
+                 * 
+                 * notificar al room _warehouse de un nuevo pedido (agregarlo)
+                 * removerlo del room CHECKIN
+                 * notificar a _admin de que una orden cambio de status
+                 * 
+                 */
+
+                    socket.to(`PRV_${branch}_admin`).to(`PRV_${branch}_warehouse`).emit('order_changestate', data);
+                break;
         
-        // console.log(gdata);
-        preventa.in(roomdash).emit( 'order_creating', { user:user } );
+            default:
+                    console.log("La orden cambio a un status no registrado!!");
+                break;
+        }
+
+        // console.log(`La orden ${order} de ${branch} ha cambiado a ${state.name}, se notificara a ${room} `);
+        // socket.in(room).emit('order_changestate',{ data });
     });
 
-    pv.on('order_created', gdata =>{
-        console.log("Nuevo pedido creado");
-        let room = `PRV-${gdata.room}`;
-        let roomdash = `DASHPRV-${gdata.room}`;
-
-        // console.log(gdata);
-        preventa.in(roomdash).emit( 'order_created', { user:user } );
+    socket.on('disconnect', data =>{
+        
+        console.log("\nun usuario abandono el CANAL PREVENTA\n");
     });
-
-    pv.on('leave',()=>{
-        console.log('Saliendo de los rooms preventa...');
-        pv.disconnect();
-    });
-
-    pv.on('disconnect',()=>{ console.log("\nun usuario abandono el ROOM preventa\n"); });
 });
 
 resurtidos.on('connection',dashboard=>{

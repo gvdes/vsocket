0ad60cfaefeed30541e9414b7799d411500a626b
hugomers <hugomo0897@gmail.com>
2024-05-13 16:48:20 -0600

    se agrega pureto 9000

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const allowedSites = [
     'http://localhost:2100',
     'http://localhost:2200',// para que se conecte al restock
     'http://192.168.10.112:2200',// para que se conecte al restock
+    'http://192.168.10.112:9000',// para que se conecte al restock
 
     'http://192.168.10.189:2200',
     'http://192.168.10.189:7007',

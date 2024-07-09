43f352a5149c73dc53cc59e829f551c0c3a778ff
hugomers <hugomo0897@gmail.com>
2024-07-09 11:36:57 -0600

    se agrega la ip local

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const allowedSites = [
     'http://localhost:2100',
     'http://localhost:9000',
     'http://localhost:2200',// para que se conecte al restock
+    'http://localhost:8080',// para que se conecte al restock
+    // 'http://localhost:2200',// para que se conecte al restock
+
     'http://192.168.10.112:2200',// para que se conecte al restock
     'http://192.168.10.112:9000',// para que se conecte al restock
 
     'http://192.168.10.189:2200',
     'http://192.168.10.112:9000',
+    'http://192.168.10.112:8080',
 
     'http://192.168.10.189:7007',
     'http://192.168.10.189:6699',

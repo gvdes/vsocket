d1e8cef54110fda630f1fa65770b78a1607a46fe
HUGO MUÑOZ <hugomo0897@gmail.com>
2024-10-07 13:15:42 -0600

    se agrega el puerto 80 ?

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // ❰❰❰❰❰ D E S A R R O L L O ❱❱❱❱❱
 const allowedSites = [
     'http://vizuru-cedis-cjvmhnwzgk.dynamic-m.com:7007',
-    'http://10.20.10.21:8080'
+    'http://10.20.10.21:8080',
+    'http://10.20.10.21:80'
+
 ];
 
 console.log("Reiniciado...");

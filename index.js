426b33f7a970cfb4f8886cc55693b37295f153c6
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-08-16 10:12:29 -0600

    se agrega origen de el socket

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const allowedSites = [
     'http://192.168.10.238:2314',
     'http://192.168.10.238:1308',
     'http://192.168.12.183:2100',
+    'http://192.168.40.214:1619',
     'http://mx100-cedis-mkrqpwcczk.dynamic-m.com:4546',
     'http://mx100-cedis-mkrqpwcczk.dynamic-m.com:6006'
 ];

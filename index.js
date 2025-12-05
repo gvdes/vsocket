50b16963143310e7791a097bea98d24ddded8760
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-12-04 20:01:18 -0600

    se agrega el modulo para el ciclico

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const allowedSites = [
 console.log("Reiniciado...");
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http,{ cors:{ origin:allowedSites } });
+var io = require('socket.io')(http, { cors: { origin: allowedSites } });
 const port = 4141;
 // var io = require('socket.io')(http,{ cors:{ origin:allowedSites, credentials: false } });
 // instrument(io, { auth: false });
@@ -42,32 +42,30 @@ const clients = [];
 
 let time = time => `${time.getFullYear()}-${time.getMonth()}-${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
 
-io.on('connection', socket =>{
+io.on('connection', socket => {
     console.log("\n\n\n ===================================================================================== ");
     console.log(` === ❰❰❰❰❰ [${time(new Date())}] ${socket.id} has been connected in GSOCKET!!! ❯❯❯❯❯ ===`);
     console.log(" ===================================================================================== \n\n\n");
 });
 
-counters.on('connection',counter=>{
+counters.on('connection', counter => {
     console.log("\n\n\n\n\n=============================================");
     console.log(`[${time(new Date())}]: ❰❰❰❰❰ Nueva conexion a INVENTARIOS ❯❯❯❯❯`);
 
-    counter.on('index',gdata=>{ console.log(`[${time(new Date())}]: ${gdata.me.names} ${gdata.me.surname_pat} [${gdata.me.nick}] de ${gdata.workpoint.name} [${gdata.workpoint.alias}] ha ingresado al home de Inventarios`); });
+    counter.on('index', gdata => { console.log(`[${time(new Date())}]: ${gdata.me.names} ${gdata.me.surname_pat} [${gdata.me.nick}] de ${gdata.workpoint.name} [${gdata.workpoint.alias}] ha ingresado al home de Inventarios`); });
 
-    counter.on('joinat',gdata=>{
+    counter.on('joinat', gdata => {
         let room = gdata.room;
         let user = gdata.user;
-
         let msg = `${user.me.names} ${user.me.surname_pat} [${user.me.nick}] de ${user.workpoint.name} [${user.workpoint.alias}]`;
-
         console.log(`Uniendo a sala: ${room}`);
         counter.join(room);
         console.log(`${msg} se ha unido al ROOM: ${room}`);
-        counters.in(room).emit('joined',user);
+        counters.in(room).emit('joined', user);
         console.log("\n=============================================");
     });
 
-    counter.on('counting',gdata=>{
+    counter.on('counting', gdata => {
         let user = gdata.by;
         let product = gdata.product;
         let room = gdata.room;
@@ -75,10 +73,10 @@ counters.on('connection',counter=>{
         let msg = `${user.me.names} ${user.me.surname_pat} [${user.me.nick}] de ${user.workpoint.name} [${user.workpoint.alias}]`;
         console.log(`${msg} esta contando un elemento`);
 
-        counter.broadcast.to(room).emit('counting',{by:user, product:product});
+        counter.broadcast.to(room).emit('counting', { by: user, product: product });
     });
 
-    counter.on('cancelcounting',gdata=>{
+    counter.on('cancelcounting', gdata => {
         // console.log(gdata);
         let user = gdata.by;
         let product = gdata.product;
@@ -86,10 +84,10 @@ counters.on('connection',counter=>{
 
         let msg = `${user.me.names} ${user.me.surname_pat} [${user.me.nick}] de ${user.workpoint.name} [${user.workpoint.alias}]`;
         console.log(`${msg} ha cancelado el conteo en el ROOM: ${room}`);
-        counter.broadcast.to(room).emit('cancelcounting',{by:user, product:product});
+        counter.broadcast.to(room).emit('cancelcounting', { by: user, product: product });
     });
 
-    counter.on('countingconfirmed',gdata=>{
+    counter.on('countingconfirmed', gdata => {
         // console.log(gdata);
         let user = gdata.by;
         let product = gdata.product;
@@ -97,19 +95,27 @@ counters.on('connection',counter=>{
         let settings = gdata.settings;
         let msg = `${user.me.names} ${user.me.surname_pat} [${user.me.nick}] de ${user.workpoint.name} [${user.workpoint.alias}]`;
         console.log(`${msg} ha confirmado el conteo en el ROOM: ${room}`);
-
-        counter.broadcast.to(room).emit('countingconfirmed',{by:user, product:product, settings:settings});
+        counter.broadcast.to(room).emit('countingconfirmed', { by: user, product: product, settings: settings });
     });
+    counter.on('endingCounting', gdata => {
+        let user = gdata.by;
+        let room = gdata.room;
+        let counter = gdata.counter;
+        let msg = `${user.me.names} ${user.me.surname_pat} [${user.me.nick}] de ${user.workpoint.name} [${user.workpoint.alias}]`;
+        console.log(`${msg} ha terminado el conteo ${room}`);
+        counters.in(room).emit('endingCounting', { user, counter });
+        // counter.broadcast.to(room).emit('endingCounting',{by:user});
+    })
 });
 
 // const socketsin = (nmspc,room) => { return io.of("/"+nmspc).in(room).allSockets(); }
 
-preventa.on('connection', socket =>{
+preventa.on('connection', socket => {
     console.log(" ================================================================================= ");
     console.log(` == [${time(new Date())}]: ❯❯❯❯❯ Nueva conexion a PREVENTA (${socket.id}) ==`);
     console.log(" ================================================================================= \n");
 
-    socket.emit('socketid',socket.id);
+    socket.emit('socketid', socket.id);
 
     socket.on('join', ({ profile, workpoint, room }) => {
 
@@ -121,11 +127,11 @@ preventa.on('connection', socket =>{
 
             socket.join(_room);
             console.log(`[${time(new Date())}]: ❯❯❯❯❯ ${user} de ${branch} se unio a ${_room}\n`);
-            socket.emit('joinedat', { profile, workpoint, room:_room });
+            socket.emit('joinedat', { profile, workpoint, room: _room });
 
-            socket.in(_admins).in(_room).emit('newjoin', { profile, workpoint, room:_room });
+            socket.in(_admins).in(_room).emit('newjoin', { profile, workpoint, room: _room });
         } catch (error) {
-            let newerror = { error, time:time(new Date()), msg:'No se pudo leer la propiedad solicitada' };
+            let newerror = { error, time: time(new Date()), msg: 'No se pudo leer la propiedad solicitada' };
             let msgsave = JSON.stringify(newerror);
             // fs.writeFileSync('error_log.json',msgsave);
         }
@@ -138,16 +144,16 @@ preventa.on('connection', socket =>{
             let branch = workpoint.alias;
             let _room = `PRV_${workpoint.alias}_${room}`;
 
-            socket.emit('unjoined', { profile, workpoint, room:_room });
-            socket.to(_room).emit('socketunjoined', { profile, workpoint, room:_room });
+            socket.emit('unjoined', { profile, workpoint, room: _room });
+            socket.to(_room).emit('socketunjoined', { profile, workpoint, room: _room });
             socket.leave(_room);
 
             console.log(`[${time(new Date())}]: ❰❰❰❰❰ ${nick} de ${branch} salio de ${_room}\n`);
 
         } catch (error) {
-            socket.emit('unjoin_crash',{error});
+            socket.emit('unjoin_crash', { error });
 
-            let newerror = { error, time:time(new Date()), msg:'No se pudo leer la propiedad solicitada' };
+            let newerror = { error, time: time(new Date()), msg: 'No se pudo leer la propiedad solicitada' };
             let msgsave = JSON.stringify(newerror);
             // fs.writeFileSync('error_log.json',msgsave);
         }
@@ -159,14 +165,14 @@ preventa.on('connection', socket =>{
             let order = data.order.id;
             let by = data.order.created_by.nick;
             let branch = data.order.from.alias;
-    
+
             let _admins = `PRV_${branch}_admins`;
             let _sales = `PRV_${branch}_sales`;
-    
+
             console.log(`[${time(new Date())}]: --❯ ${by} de ${branch} creo el pedido ${order} (${data.order.name})\n`);
             socket.to(_admins).to(_sales).emit('order_add', data);
         } catch (error) {
-            let newerror = { error, time:time(new Date()), msg:'No se pudo leer la propiedad solicitada' };
+            let newerror = { error, time: time(new Date()), msg: 'No se pudo leer la propiedad solicitada' };
             let msgsave = JSON.stringify(newerror);
         }
     });
@@ -207,7 +213,7 @@ preventa.on('connection', socket =>{
                  * notificar al room _checkin de un nuevo pedido (agregarlo)
                  */
                 socket.to(_checkin).emit('order_add', data);
-            break;
+                break;
 
             case 4:
                 /**
@@ -217,43 +223,43 @@ preventa.on('connection', socket =>{
                  */
                 socket.to(_supply).emit('order_add', data);
                 socket.to(_checkin).emit('order_update', data);
-            break;
+                break;
 
             case 5: case 7:
                 socket.to(_supply).to(_checkout).emit('order_aou', data);
-            break;
+                break;
 
             case 100:
                 socket.to(_cfg).emit('order_update', data);
-            break;
+                break;
 
             default:
                 console.log("La orden cambio a un status no registrado!!");
-            break;
+                break;
         }
     });
 
-    socket.on('module_update', ({profile,workpoint,state}) => {
+    socket.on('module_update', ({ profile, workpoint, state }) => {
         let by = profile.me.nick;
         let branch = workpoint.alias;
-        let _msgstate = state.state ? 'encendio':'apago';
+        let _msgstate = state.state ? 'encendio' : 'apago';
         let _room = `PRV_${workpoint.alias}_cfg`;
 
         console.log(`[${time(new Date())}]: --❯ ${by} de ${branch} ${_msgstate} el modulo ${state.name}.\n`);
-        socket.to(_room).emit('module_update',{by,_msgstate,state});
+        socket.to(_room).emit('module_update', { by, _msgstate, state });
     });
 
-    socket.on('cash_update', ({profile,workpoint,cash,newstate}) => {
+    socket.on('cash_update', ({ profile, workpoint, cash, newstate }) => {
         let by = profile.me.nick;
         let branch = workpoint.alias;
-        let _msgstate = newstate.id==1 ? `${by} encendio la ${cash.name}`:`${by} apago la ${cash.name}`;
+        let _msgstate = newstate.id == 1 ? `${by} encendio la ${cash.name}` : `${by} apago la ${cash.name}`;
         let _room = `PRV_${branch}_cfg`;
 
         console.log(`[${time(new Date())}]: --❯ ${_msgstate}.\n`);
-        socket.to(_room).emit('cash_update',{by,cash,newstate});
+        socket.to(_room).emit('cash_update', { by, cash, newstate });
     });
 
-    socket.on('disconnect', data =>{
+    socket.on('disconnect', data => {
         console.log("\nun usuario abandono el CANAL PREVENTA\n");
     });
 });
@@ -270,15 +276,15 @@ preventa.on('connection', socket =>{
 //     dashboard.on('disconnect',()=>{ console.log("\nun usuario abandono el canal resurtidos\n"); });
 // });
 
-resurtidos.on('connection',dashboard=>{
+resurtidos.on('connection', dashboard => {
     console.log("\n\n\n\n\n=============================================");
     console.log("❰❰❰❰❰ Nueva conexion a RESURTIDOS ❯❯❯❯❯");
 
-    dashboard.on('joinat',gdata=>{
+    dashboard.on('joinat', gdata => {
         console.log(gdata);
         //let roomdash = `DASHREQ-${gdata.room}`;// nombre del room a unirse
         let roomdash = `DASHBOARDSREQS`;// nombre del room a unirse
-        console.log("uniendose a room "+roomdash);//
+        console.log("uniendose a room " + roomdash);//
         let user = gdata.profile;
         let from = gdata.workpoint;
         dashboard.join(roomdash);
@@ -289,77 +295,77 @@ resurtidos.on('connection',dashboard=>{
         // dashboard.to(roomdash).emit( 'joineddashreq', {user:user, notify:msg } );
 
         // envia a todos en el room, INCLUYENDO al emisor
-        resurtidos.to(roomdash).emit( 'joineddashreq', { user:user, notify:msg, fromdashboard:gdata.isdashboard, from:from } );
+        resurtidos.to(roomdash).emit('joineddashreq', { user: user, notify: msg, fromdashboard: gdata.isdashboard, from: from });
         console.log("\n=============================================");
     });
 
-    dashboard.on('creating',gdata=>{
+    dashboard.on('creating', gdata => {
         let roomdash = `DASHBOARDSREQS`;// nombre del room a unirse
         console.log("Se esta levantando un pedido");
         console.log(roomdash);
         console.log(gdata);
-        resurtidos.to(roomdash).emit('creating',gdata);
+        resurtidos.to(roomdash).emit('creating', gdata);
     });
 
-    dashboard.on('order_open',gdata=>{
+    dashboard.on('order_open', gdata => {
         console.log(gdata);
         let roomdash = `DASHBOARDSREQS`;
         console.log(gdata.cmd);
-        resurtidos.to(roomdash).emit('order_open',gdata);
+        resurtidos.to(roomdash).emit('order_open', gdata);
     });
 
-    dashboard.on('order_update',gdata=>{
+    dashboard.on('order_update', gdata => {
         console.log(gdata);
         let roomdash = `DASHBOARDSREQS`;
         // console.log(`${gdata.profile.me.nick} abrio la orden ${gdata.order.id}`);
-        resurtidos.to(roomdash).emit('order_update',gdata);
+        resurtidos.to(roomdash).emit('order_update', gdata);
     });
 
-    dashboard.on('order_changestate',gdata=>{
+    dashboard.on('order_changestate', gdata => {
         console.log("actualizando pedido...");
         let roomdash = `DASHBOARDSREQS`;
         // console.log(`${gdata.profile.me.nick} de ${gdata.profile.workpoint.alias} ha cambiado el status de la orden ${gdata.order.id} a ${gdata.state.id} (${gdata.state.name})`);
 
-        resurtidos.to(roomdash).emit('order_changestate',gdata);
+        resurtidos.to(roomdash).emit('order_changestate', gdata);
     });
 
-    dashboard.on('order_refresh',gdata=>{
+    dashboard.on('order_refresh', gdata => {
         console.log("refrescando pedido...");
         let roomdash = `DASHBOARDSREQS`;
         // // console.log(`${gdata.profile.me.nick} de ${gdata.profile.workpoint.alias} ha cambiado el status de la orden ${gdata.order.id} a ${gdata.state.id} (${gdata.state.name})`);
 
-        resurtidos.to(roomdash).emit('order_refresh',gdata);
+        resurtidos.to(roomdash).emit('order_refresh', gdata);
     });
 
     // orderpartition_refresh
 
-    dashboard.on('orderpartition_refresh',gdata=>{
+    dashboard.on('orderpartition_refresh', gdata => {
         console.log("refrescando pedido...");
         let roomdash = `DASHBOARDSREQS`;
         // // console.log(`${gdata.profile.me.nick} de ${gdata.profile.workpoint.alias} ha cambiado el status de la orden ${gdata.order.id} a ${gdata.state.id} (${gdata.state.name})`);
 
-        resurtidos.to(roomdash).emit('orderpartition_refresh',gdata);
+        resurtidos.to(roomdash).emit('orderpartition_refresh', gdata);
     });
-    dashboard.on('ChangeStatus',gdata=>{
+    dashboard.on('ChangeStatus', gdata => {
         console.log("Refrescar Estado...");
         let roomdash = `DASHBOARDSREQS`;
         // // console.log(`${gdata.profile.me.nick} de ${gdata.profile.workpoint.alias} ha cambiado el status de la orden ${gdata.order.id} a ${gdata.state.id} (${gdata.state.name})`);
-        resurtidos.to(roomdash).emit('ChangeStatus',gdata);
+        resurtidos.to(roomdash).emit('ChangeStatus', gdata);
     });
 
-    dashboard.on('partitionCreate',gdata=>{
+    dashboard.on('partitionCreate', gdata => {
         console.log("Creando Particion...");
         let roomdash = `DASHBOARDSREQS`;
-        resurtidos.to(roomdash).emit('sktPartitionCreate',gdata);
+        resurtidos.to(roomdash).emit('sktPartitionCreate', gdata);
     })
 
-    dashboard.on('partitionDelete',gdata=>{
+    dashboard.on('partitionDelete', gdata => {
         console.log("Eliminando Particion...");
         let roomdash = `DASHBOARDSREQS`;
-        resurtidos.to(roomdash).emit('sktPartitionDelete',gdata);
+        resurtidos.to(roomdash).emit('sktPartitionDelete', gdata);
     })
 
-    dashboard.on('leave',(gdata)=>{
+    dashboard.on('leave', (gdata) => {
         // console.log(gdata);
         console.log('Saliendo de los rooms resurtidos...');
         dashboard.disconnect();
@@ -374,29 +380,29 @@ resurtidos.on('connection',dashboard=>{
     //     resurtidos.emit('changeStatusPartition',chst)
     // })
 
-    dashboard.on('checkouConect',(params)=> {
+    dashboard.on('checkouConect', (params) => {
         let origen = params._workpoint_to;
         let destino = params._workpoint_from;
         let requisision = params.id
-        let condicion = `${origen}` 
+        let condicion = `${origen}`
         console.log(condicion)
         dashboard.join(condicion)
 
     })
-    dashboard.on('blockButton',(param) => {
-        let room  = `${param._workpoint_to}`
+    dashboard.on('blockButton', (param) => {
+        let room = `${param._workpoint_to}`
         console.log(room)
         resurtidos.to(room).emit('blockButton');
-        
-      })
-      dashboard.on('unblockButton',(param) => {
-        let room  = `${param._workpoint_to}`
+
+    })
+    dashboard.on('unblockButton', (param) => {
+        let room = `${param._workpoint_to}`
         console.log(room)
         resurtidos.to(room).emit('unblockButton');
-        
-      })
 
-    dashboard.on('disconnect',()=>{ console.log("\nun usuario abandono el canal resurtidos\n"); });
+    })
+
+    dashboard.on('disconnect', () => { console.log("\nun usuario abandono el canal resurtidos\n"); });
 });
 
 
